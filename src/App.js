7163d8e49562e6958a4cff6336c61dd74919c24a
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-03 16:38:22 -0400

    Global Style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes />
     </BrowserRouter>
   );
