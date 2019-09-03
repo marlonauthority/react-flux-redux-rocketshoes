09a449915ed32aada7c0169e2af33f3b5991cadb
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-03 16:50:51 -0400

    Header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
+      <Header />
       <Routes />
     </BrowserRouter>
   );
