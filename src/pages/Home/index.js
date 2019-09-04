537c2fda447924eb4e122e51b9906dee0b268735
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-03 23:22:54 -0400

    Reducer ADD_TO_CART

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+// Nome auto sugestivo, eé uma funcao que fara o component se conectar ao redux, observe na ultima linha da deste component
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+ class Home extends Component {
   state = {
     products : [],
 
@@ -24,6 +26,18 @@ export default class Home extends Component {
       products: data
     });
   }
+
+  handleAddProduct = product => {
+    // todo component que se conecta ao redux recebe uma propriedade dispatch
+    const { dispatch } = this.props;
+    // dispatch dispara uma Action ao Redux aqui por exemplo queremos adcionar um product ao carrinho
+    // -> Action propriamente dita, lembrando que eé necessario um "type"
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    })
+  }
+
   render() {
     const { products } = this.state;
 
@@ -39,7 +53,7 @@ export default class Home extends Component {
         <strong>{product.title}</strong>
         <span>{product.priceFormatted}</span>
 
-        <button type="button">
+        <button type="button" onClick={() => this.handleAddProduct(product)}>
           <div>
             <MdAddShoppingCart size={16} color="#fff" /> 3
           </div>
@@ -53,3 +67,6 @@ export default class Home extends Component {
     )
   }
 }
+
+// exporta-se o componente passando como uma 2ª funcao o nome do component, deste modo pode-se fazer o uso do redux
+export default connect()(Home);
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,14 @@
-export default function cart() {
-  return [];
+// todo reducer recebe um state que é estado inicial"aqui ê um array vazio" e recebe tambem uma action que dira o que deve ser feito
+export default function cart(state = [], action) {
+  // aqui ficará ouvindo a action
+  switch (action.type) {
+    // caso a action disparada "dispatch" for igual a:
+    case 'ADD_TO_CART':
+      // retorna um novo State
+      // aqui pegamos tudo o que ja existe "..." e adicionamos mais o que veio da action
+      return [...state, action.product];
+    // Estado default caso nao tenha acontecido NADA
+    default:
+      return state;
+  }
 }
