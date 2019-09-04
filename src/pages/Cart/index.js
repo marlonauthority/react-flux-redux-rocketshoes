f51dcf74dc2cfb030f7268473fe6d0c921fa7bcd
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-04 19:10:23 -0400

    Removendo item do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -7,8 +7,7 @@ import {
 } from 'react-icons/md';
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
-  console.tron.log(cart);
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -46,7 +45,12 @@ function Cart({ cart }) {
                 <strong>R$258,80</strong>
               </td>
               <td>
-                <button type="button">
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                  }
+                >
                   <MdDelete size={20} color="#7159c1" />
                 </button>
               </td>
