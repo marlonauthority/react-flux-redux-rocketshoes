07f669d08e86ebea15997d291707acd92326e0b7
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-04 17:04:29 -0400

    Produto duplicado

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,20 +1,19 @@
+import produce from 'immer';
+
 // todo reducer recebe um state que é estado inicial"aqui ê um array vazio" e recebe tambem uma action que dira o que deve ser feito
 export default function cart(state = [], action) {
   // aqui ficará ouvindo a action
   switch (action.type) {
     // caso a action disparada "dispatch" for igual a:
     case 'ADD_TO_CART':
-      // retorna um novo State
-      // aqui pegamos tudo o que ja existe "..." e podemos manter o state assim ou alterar ele adiconando mais coisa, como abaixo:
-      return [
-        ...state,
-        {
-          // copia todas as informacoes que vem de dentro do product
-          ...action.product,
-          // adciona mais um informacao ao array
-          amount: 1,
-        },
-      ];
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({ ...action.product, amount: 1 });
+        }
+      });
     // Estado default caso nao tenha acontecido NADA
     default:
       return state;
