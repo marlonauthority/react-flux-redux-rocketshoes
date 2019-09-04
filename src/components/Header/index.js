29577ecfa1a9e6bff00de3f560347d303e71f5e8
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-03 23:34:34 -0400

    exibindo qtd de itens no carrinho no header do app

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { MdShoppingBasket } from 'react-icons/md';
 import { Container, Cart } from './styles';
 import logo from '../../assets/images/logo.svg';
 
-function Header() {
+function Header(props) {
+  // console.log(props.cart);
+  const cartSize = props.cart.length;
+
   return (
     <Container>
       <Link to="/">
@@ -14,11 +18,20 @@ function Header() {
       <Cart to="/cart">
         <div>
           <strong>Meu Carrinho</strong>
-          <span>3 itens</span>
+          <span>{cartSize} itens</span>
         </div>
         <MdShoppingBasket size={36} color="#fff" />
       </Cart>
     </Container>
   );
 }
-export default Header;
+
+/**
+ * o connect pode receber parametros, a primeira é uma funcao
+ *  por exemplo aqui recemos um estado inteiro do redux, e as informacoes que queira acessar
+ * estao acessiveis usando um objeto ({ })
+ * */
+export default connect(state => ({
+  // criamos um objeto, com o conteudo do reducer a ser acessado, esse nome esta la no rootReducer.js
+  cart: state.cart,
+}))(Header);
