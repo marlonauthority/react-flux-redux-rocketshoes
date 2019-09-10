539969e8cdd9632065751487ec007fd32330948b
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-09 20:33:35 -0400

    att React Hooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,85 +1,60 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 // Nome auto sugestivo, eé uma funcao que fara o component se conectar ao redux, observe na ultima linha da deste component
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { FaSpinner } from 'react-icons/fa';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 // Actions
 import * as CartActions from '../../store/modules/cart/actions';
 
 import { ProductList } from './styles';
-import { FaSpinner } from 'react-icons/fa';
 
- class Home extends Component {
-  state = {
-    products : [],
-    loading: true,
+function Home({ amount, addToCartRequest }) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await api.get(`/products`);
+
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }));
+
+      setProducts(data);
+      setLoading(false);
+    }
+    loadProducts();
+  }, []);
+
+  function handleAddProduct(id) {
+    addToCartRequest(id);
   }
 
-  async componentDidMount() {
+  return (
+    <ProductList loading={loading}>
+      {loading === true ? <FaSpinner color="#fff" size={24} /> : null}
 
-    const response = await api.get(`/products`);
-    // formata o preco antes de ir para o render
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
 
-    const data = response.data.map(product => ({
-      ...product,
-      priceFormatted: formatPrice(product.price),
-    }));
-
-    this.setState({
-      products: data,
-      loading: false,
-    });
-  }
-
-  handleAddProduct = id => {
-    const { addToCartRequest } = this.props;
-    addToCartRequest(id)
-  }
-
-  render() {
-    const { products, loading } = this.state;
-    const { amount } = this.props
-
-
-    return (
-
-
-
-      <ProductList loading={loading}>
-        {loading === true ? (
-              <FaSpinner color="#fff" size={24} />
-            ) : (
-              null
-            )}
-
-
-        { products.map(product => (
-          <li key={product.id}>
-        <img
-          src={product.image}
-          alt={product.title}
-        />
-        <strong>{product.title}</strong>
-        <span>{product.priceFormatted}</span>
-
-        <button type="button" onClick={() => this.handleAddProduct(product.id)}>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-           {amount[product.id] || 0}
-
-
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-        )) }
-
+          <button type="button" onClick={() => handleAddProduct(product.id)}>
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" />
+              {amount[product.id] || 0}
+            </div>
+            <span>Adicionar ao carrinho</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
-
-    )
-  }
+  );
 }
 
 const mapStateToProps = state => ({
@@ -96,4 +71,7 @@ const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
 // exporta-se o componente passando como uma 2ª funcao o nome do component, deste modo pode-se fazer o uso do redux
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
